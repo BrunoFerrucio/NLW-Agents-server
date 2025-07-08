1f6e102ecf6594fae6fda43abd279d9f95aacaec
Bruno Ferrucio <brunofas1@gmail.com>
2025-07-08 13:40:41 -0300

    Add Biome (https://www.ultracite.ai/introduction)

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
-import fastifyCors from "@fastify/cors";
-import fastify from "fastify";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import fastifyCors from '@fastify/cors';
+import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: 'http://localhost:8888',
-})
+});
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running on http://localhost:3333");
-})
+app.get('/health', () => {
+  return 'OK';
+});
+
+app.listen({ port: env.PORT });
